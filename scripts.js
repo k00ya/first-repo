127d6ec4274fc7683dfd44759d7ccde9a0a65900
k00ya <133164751+k00ya@users.noreply.github.com>
2023-08-10 07:47:35 -0700

    added new features such as dancing dude and reorganized subheaders

--- scripts.js
+++ scripts.js
@@ -20,3 +20,13 @@ function playAudio() {
     audio.currentTime = 0; // Resets the audio to the beginning
   }
   
+  var isawHeader = document.getElementById('isaw-header');
+var isawGif = document.querySelector('.isaw-gif');
+
+isawHeader.addEventListener('mouseover', function() {
+    isawGif.style.opacity = '1'; // Fade in on hover over Isaw header
+});
+
+isawHeader.addEventListener('mouseout', function() {
+    isawGif.style.opacity = '0'; // Fade out on hover out
+});
