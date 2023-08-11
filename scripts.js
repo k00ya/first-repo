1f89c46d5ab9e06122e68a74fb726c1e84a25d0f
k00ya <133164751+k00ya@users.noreply.github.com>
2023-08-11 04:03:43 -0700

    deleted old audio // added new audio // added rain feature // added new gifs

--- scripts.js
+++ scripts.js
@@ -1,38 +1,51 @@
 function toggleAudio(audioId, play) {
     var audio = document.getElementById(audioId);
-    if (play) {
-      audio.volume = 0.3; // You can change this value to whatever you prefer
-      audio.play();
-    } else {
-      audio.pause();
-      audio.currentTime = 0;
-    }
+    play ? audio.play() : audio.pause();
+    audio.currentTime = play ? audio.currentTime : 0;
+    audio.volume = 0.3;
   }
   
+  function handleGifOpacity(headerClass, gifClass, audioId) {
+    var header = document.querySelector(headerClass);
+    var gif = document.querySelector(gifClass);
+    gif.style.opacity = '0';
+    header.addEventListener('mouseover', () => {
+      toggleAudio(audioId, true);
+      gif.style.cssText = 'opacity: 1; transition: opacity 0.5s ease;';
+    });
+    header.addEventListener('mouseout', () => {
+      toggleAudio(audioId, false);
+      gif.style.opacity = '0'; // Fix to fade out the gif when not hovered
+    });
+  }  
+  
+  function createRain() {
+    var div = document.createElement('div');
+    div.className = 'rain show';
+    for (var i = 0; i < 100; i++) {
+      var drop = document.createElement('div');
+      drop.className = 'drop';
+      drop.style.cssText = `left: ${(Math.random() * 100)}vw; animation-duration: ${(Math.random() * 1 + 0.5)}s; animation-delay: ${(Math.random() * 2)}s;`;
+      div.appendChild(drop);
+    }
+    return div;
+  }
+  
+  var rainDiv;
   var balutHeader = document.querySelector('.header-balut');
-  var isawHeader = document.querySelector('.header-isaw');
-  var isawGif = document.querySelector('.isaw-gif');
-  
-  isawGif.style.opacity = '0'; // Set initial opacity to 0
-  
-  isawHeader.addEventListener('mouseover', function() {
-    toggleAudio('bibo', true);
-    isawGif.style.opacity = '1'; // Sets opacity to 1 when hovered
-    isawGif.style.transition = 'opacity 0.5s ease'; // Adds a smooth transition effect
+  balutHeader.addEventListener('mouseover', () => {
+    rainDiv = createRain();
+    document.body.appendChild(rainDiv);
+    toggleAudio('kiss', true);
+  });
+  balutHeader.addEventListener('mouseout', () => {
+    rainDiv.className = 'rain';
+    rainDiv.addEventListener('transitionend', () => rainDiv.remove());
+    toggleAudio('kiss', false);
   });
   
-  isawHeader.addEventListener('mouseout', function() {
-    toggleAudio('bibo', false);
-    isawGif.style.opacity = '0'; // Sets opacity back to 0 when not hovered
-  });
-  
-  balutHeader.addEventListener('mouseover', function() {
-    toggleAudio('eyes', true);
-  });
-  
-  balutHeader.addEventListener('mouseout', function() {
-    toggleAudio('eyes', false);
-  });
+  handleGifOpacity('.header-isaw', '.isaw-gif', 'bibo');
+  handleGifOpacity('.header-isaw', '.right-gif', 'bibo');
   
   
   
