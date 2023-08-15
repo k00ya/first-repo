f1aef50b0be44eb491138fd435eb0490b0fbcb26
k00ya <133164751+k00ya@users.noreply.github.com>
2023-08-14 18:10:46 -0700

    added new audio and fixed bugs

--- scripts.js
+++ scripts.js
@@ -1,5 +1,40 @@
 function toggleAudio(audioId, play) {
     var audio = document.getElementById(audioId);
+    if (play) {
+      audio.volume = 0.3; // You can change this value to whatever you prefer
+      audio.play();
+    } else {
+      audio.pause();
+      audio.currentTime = 0;
+    }
+  }
+  
+  var balutHeader = document.querySelector('.header-balut');
+  var isawHeader = document.querySelector('.header-isaw');
+  var isawGif = document.querySelector('.isaw-gif');
+  
+  isawGif.style.opacity = '0'; // Set initial opacity to 0
+  
+  isawHeader.addEventListener('mouseover', function() {
+    toggleAudio('bibo', true);
+    isawGif.style.opacity = '1'; // Sets opacity to 1 when hovered
+    isawGif.style.transition = 'opacity 0.5s ease'; // Adds a smooth transition effect
+  });
+  
+  isawHeader.addEventListener('mouseout', function() {
+    toggleAudio('bibo', false);
+    isawGif.style.opacity = '0'; // Sets opacity back to 0 when not hovered
+  });
+  
+  balutHeader.addEventListener('mouseover', function() {
+    toggleAudio('eyes', true);
+  });
+  
+  balutHeader.addEventListener('mouseout', function() {
+    toggleAudio('eyes', false);
+  });
+  function toggleAudio(audioId, play) {
+    var audio = document.getElementById(audioId);
     play ? audio.play() : audio.pause();
     audio.currentTime = play ? audio.currentTime : 0;
     audio.volume = 0.3;
@@ -61,12 +96,6 @@ function toggleAudio(audioId, play) {
   }
   
   randomizeKwekwekColors();
-  
-  
-  
-  
-  
-  
   
   
   
